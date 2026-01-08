46775ade193dcb22b13c7345edb5d116dd9baec3
Timur <huntertring87@gmail.com>
2026-01-08 22:03:37 +0300

    initial commit

---
+++ Assets/Resources/CharacterCharacteristicsResource.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name CharacterCharacteristicsResource
+
+@export var max_health = 100
+@export var damage = 1
+@export var defense = 1
+@export var evasive_chance = 1
+@export var attack_speed = 1
